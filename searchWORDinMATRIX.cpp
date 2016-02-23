1229812a8be11eb1c72e76defbdfd38c0ccac334
Abhinav Mishra <mishra88abhinav@gmail.com>
2016-02-23 12:02:39 -0700

    Update searchWORDinMATRIX.cpp

--- searchWORDinMATRIX.cpp
+++ searchWORDinMATRIX.cpp
@@ -1,6 +1,7 @@
 class Solution {
 
 private:
+//this variable takes care of the starting point and then corresponding path in the matrix
 int path=1;
 
 public:
@@ -8,9 +9,11 @@ public:
 
     bool existUtil(vector<vector<char> >& board, int x, int y, int index, string word, vector<vector<int> >& vec){
         
+        // if any of the below condition evaluates to true we do not have to go further
         if( x<0 || x>= board.size() || y<0 || y>=board[0].size() || board[x][y]!=word[index] || vec[x][y] !=0 )
             return false;
-            
+        
+        // if it is the last index we have found the word .. return true    
         if(index == word.size()-1)
             {
                 vec[x][y] = path++;
@@ -21,18 +24,23 @@ public:
             
             vec[x][y] = path++;
             
+            // check the sale column next row
             if(existUtil(board,x+1,y,index+1,word,vec))
                 return true;
-                
+            
+            //check the same row and next column    
             if(existUtil(board,x,y+1,index+1,word,vec))
                 return true;
-                
+            
+            // check the current column and previous row
             if(existUtil(board,x-1,y,index+1,word,vec))
                 return true;
-        
+            
+            // check the current row and previous column
             if(existUtil(board,x,y-1,index+1,word,vec))
                 return true;
-                
+            
+            // there was a character mismatch put a 0 in the output matrix, decrement path by 1 and return false   
             vec[x][y] = 0;
             path--;
             return false;
@@ -45,6 +53,7 @@ public:
         int m = board.size();
         int n=board[0].size();
         
+        // this is a solution matrix which will have a non zero value if a character is matched otherwise zero
         vec.resize(m,vector<int>(n,0));
         
         for(int i=0;i<m;i++){
